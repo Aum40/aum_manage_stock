9d0538e57d75573b580a5f4002dafcb6040145c8
dewpm <dew.msit06@gmail.com>
2026-08-19 14:21:26 +0700

    chore(api): upgrade to Prisma 7

--- api/src/database/prisma.service.ts
+++ api/src/database/prisma.service.ts
@@ -1,4 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../generated/prisma-client/client';
 
 @Injectable()
@@ -6,6 +8,14 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor(config: ConfigService) {
+    super({
+      adapter: new PrismaPg({
+        connectionString: config.getOrThrow<string>('DATABASE_URL'),
+      }),
+    });
+  }
+
   async onModuleInit(): Promise<void> {
     await this.$connect();
   }
